67d71c7b2d3cb5964476668083977000fccbe036
CoachLAMartin <148378847+CoachLAMartin@users.noreply.github.com>
2023-10-19 10:43:49 -0500

    Update streamlit_app.py
    updated error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,9 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
-    
-  streamlit.write('The user entered ', fruit_choice)
+
+except URLError as e:
+    streamlit.error()
 
 # take the json version of the response and normalize it
 #output it the screen as a table
